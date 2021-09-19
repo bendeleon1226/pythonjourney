a534b540167de6dc4167c7b939ef3a7191940730
ben de leon <bendeleonjr@gmail.com>
2021-09-19 23:03:29 +0800

    initial commit

--- 001_Steven.py
+++ 001_Steven.py
@@ -1,3 +1,57 @@
-x = input("What is your favorite food? ")
-print(x + " is a nice food.")
+print("hi welcome to math class")
+print("Welcome to Easy")
+a=input("what is 1+1:")
+if a== str(1+1):
+    print("great job 9 more to go to reach medium")
+else:
+    print("Sorry you are wrong if you want to start again press stop and run")
+b=input("what is 8+1:")
+if b==str(8+1):
+    print("yay you did it")
+else:
+    print("sorry you're wrong")
+c=input("What is 2+5:")
+if c== str(2+5):
+    print("you did it 7 more to go")
+else:
+    print("sorry do again?")
+d=input("23+1:")
+if d== str(23+1):
+    print("how smart are you")
+else:
+    print("sorry you are wrong")
+e=input("what is 44+6:")
+if e== str(44+6):
+    print("correct four numbers before you reach to  Medium")
+else:
+    print("sorry you are wrong")
+f=input("50+50:")
+if f=="100":
+    print("yay you're correct")
+else:
+    print("sorry your wrong")
+g=input("80+23:")
+if  g == str(80+23):
+    print("you are correct two more are you exited")
+else:
+    print("sorry you are wrong")
+h=input("33+33:")
+if h== str(33+33):
+    print("you are correct 1 more :D")
+i=input("what is 200+1:")
+if i== str(200+1):
+    print("you're correct Welcome to Medium")
+else:
+    print("try running it again")
+j=input("67-7:")
+if j== str(67-7):
+    print("correct")
+else:
+    print("sorry your wrong")
+
+
+
+
+
+
 
